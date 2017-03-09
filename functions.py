eb490e34a336fa4908f7c5fa9b2a6c82caa06abc
Muhammet <muhammeta7@gmail.com>
2017-03-08 22:59:36 -0500

    vacation car rental spending money and total calcultion complete

--- functions.py
+++ functions.py
@@ -98,4 +98,19 @@ def plane_ride_cost(city):
     elif city=="Los Angeles":
         return 475
 
+def rental_car_cost(days):
+    cost = 40*days
+    if days >= 7:
+        return cost-50
+    elif days >=3:
+        return cost-20
+    else:
+        return cost
+        
+def trip_cost(city,days,spending_money):
+    return hotel_cost(days)+plane_ride_cost(city)+rental_car_cost(days)+spending_money
+        
+print trip_cost("Los Angeles",5,600) # Parameters input for testing
+
+
 
