cafea28f58fe0e4d55442b98089339a9e39b4d8c
Muhammet <muhammeta7@gmail.com>
2017-03-15 21:24:29 -0400

    created average functions for student calculations

--- lesson6.py
+++ lesson6.py
@@ -17,3 +17,23 @@ tyler = {
     "quizzes": [0.0, 75.0, 78.0],
     "tests": [100.0, 100.0]
 }
+
+# Print all students with grades
+students = [lloyd,alice, tyler]
+for student in students:
+    print student["name"],"\n",student["homework"],"\n",student["quizzes"],"\n",student["tests"]
+    print # Just spacing in between each student
+
+# Create average function
+def average(numbers):
+     total = sum(numbers)
+     total = float(total)
+     total = total/len(numbers)
+     return total
+
+# Get average for specific student function
+def get_average(student):
+    homework=average(student["homework"])*0.1
+    quizzes=average(student["quizzes"])*0.3
+    tests=average(student["tests"])*0.6
+    return homework+quizzes+tests
