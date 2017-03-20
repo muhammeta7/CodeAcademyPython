12259044211d521c9e20f3e345534d2fa6855658
Muhammet <muhammeta7@gmail.com>
2017-03-19 20:13:48 -0400

    completed all conditional logic for potential guesses

--- battleship.py
+++ battleship.py
@@ -30,3 +30,40 @@ def print_board(board):
         print " ".join(row)
 
 print_board(board)
+
+## Create random row and column
+from random import randint
+
+def random_row(board):
+    return randint(0, len(board)-1)
+    
+def random_col(board):
+    return randint(0, len(board)-1)
+    
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_col # Debugging purposes
+print ship_row
+
+## Allow user to guess
+guess_row = int(raw_input("Guess Row: "))
+guess_col = int(raw_input("Guess Col: "))
+
+# Conditional to check if you win
+if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"
+    
+elif board[guess_row][guess_col] == "X":
+    print "You guessed that one already."
+else:
+    print "You missed my battleship!"
+    board[guess_row][guess_col] = "X"
+    print_board(board)
+    if guess_row not in range(5) or \
+        guess_col not in range(5):
+            print "Oops, that's not even in the ocean."
+    else:
+        print "You missed my battleship!"
+        board[guess_row][guess_col]="X"
+        print_board(board)
+    
