85d647e5b5ff9d1933e72a48dc2177b84d2759ac
Muhammet <muhammeta7@gmail.com>
2017-05-29 18:10:20 -0400

    The ~ operator

--- advanced_topics.py
+++ advanced_topics.py
@@ -223,3 +223,18 @@ bits of the two numbers are 1, but not both.
 
 print bin(0b1110 ^ 0b101) # #0b1011
 
+"""
+
+The bitwise NOT operator (~) just flips all of the bits in a single number. 
+What this actually means to the computer is actually very complicated, 
+so we're not going to get into it. Just know that mathematically, this 
+is equivalent to adding one to the number and then making it negative.
+
+"""
+
+print ~1 
+print ~2
+print ~3
+print ~42
+print ~123
+
