69a20a8ae54bdc98255d9498145e9dcb1a67d6ff
Muhammet <muhammeta7@gmail.com>
2017-05-30 15:22:06 -0400

    inheritance practice

--- classes.py
+++ classes.py
@@ -138,3 +138,70 @@ my_cart = ShoppingCart("Moe")
 my_cart.add_item("salmon", 7)
 
 print my_cart
+
+# Warning: Here Be Dragons
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+# Inheritance Syntax
+class Shape(object):
+    """Makes shapes!"""
+    def __init__(self, number_of_sides):
+        self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+       self.side1 = side1
+       self.side2 = side2
+       self.side3 = side3
+
+# Override 
+# Example
+class Employee(object):
+    def __init__(self, name):
+        self.name = name
+    def greet(self, other):
+        print "Hello, %s" % other.name
+
+class CEO(Employee):
+    def greet(self, other):
+        print "Get back to work, %s!" % other.name
+
+ceo = CEO("Emily")
+emp = Employee("Steve")
+emp.greet(ceo)
+# Hello, Emily
+ceo.greet(emp)
+# Get back to work, Steve!
+
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+    def __init__(self, hours):
+        self.hours = hours
+        return hours * 12
+
+# This Looks Like a Job For
