63fbb8877c9527a552a484ab9c2e121129c46834
Muhammet <muhammeta7@gmail.com>
2017-05-30 12:04:18 -0400

    a bit more complicated lessons ^ and << exercises

--- advanced_topics.py
+++ advanced_topics.py
@@ -238,3 +238,39 @@ print ~3
 print ~42
 print ~123
 
+# =====================================================
+# A Bit More Complicated
+
+# The Man Behind the Bit Mask
+def check_bit4(input):
+    mask = 0b1000
+    desired = input & mask
+    if desired > 0 :
+        return "on"
+    else:
+        return "off"
+        
+print check_bit4(0b1001)
+
+# Turn It On
+a = 0b10111011
+mask = 0b100 
+desired =  a | mask 
+
+print(bin(desired))
+
+# Just Flip It
+a = 0b11101110
+mask = 0b11111111
+desired = a^mask
+
+print bin(desired)
+
+# Slip and Slide
+def flip_bit(number,n):
+    mask = 0b1 << (n-1)
+    result = number ^ mask
+    return bin(result)
+    
+print flip_bit(0b1001,3) #0b1101
+
