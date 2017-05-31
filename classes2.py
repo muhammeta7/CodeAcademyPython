eebcf7f05e7e054711a96fe29aa8daa98e7860d5
Muhammet <muhammeta7@gmail.com>
2017-05-31 15:11:55 -0400

    building useful classes exercise

--- classes2.py
+++ classes2.py
@@ -33,3 +33,14 @@ my_car.drive_car()
 print my_car.condition
 
 # Building Useful Classes
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+    def __repr__(self):
+        return "(%d, %d, %d)"%(self.x, self.y, self.z)
+        
+my_point = Point3D(1,2,3)
+print my_point
